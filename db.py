a4c9ac521a441543ca5cda96c8f75ec2f029bca5
Debajit Saha <debajitsahacse@gmail.com>
2026-07-13 12:34:09 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,19 +1,16 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 
 def get_connection():
-
-    """
-    Creates and returns a MySQL database connection
-    for Inventory Management System.
-    """
-
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user="####",
-            password="#####",
-            database="stockflow_db"
+            host=os.getenv("MYSQLHOST"),
+            user=os.getenv("MYSQLUSER"),
+            password=os.getenv("MYSQLPASSWORD"),
+            database=os.getenv("MYSQLDATABASE"),
+            port=int(os.getenv("MYSQLPORT", 4000)),
+            ssl_verify_cert=False
         )
 
         if conn.is_connected():
