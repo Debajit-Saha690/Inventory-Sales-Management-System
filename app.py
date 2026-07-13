8c35d58af625ba4900cb819c2f4b8ae43d406e58
Debajit Saha <debajitsahacse@gmail.com>
2026-07-13 12:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -641,6 +641,10 @@ def monthly_sales():
     )
 
 
-# RUN SERVER
+# RUN APP
+
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
